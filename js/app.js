d112b6715538b61b393bab9014732fb00aee31ac
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-06-21 21:59:49 +0600

    working with slider

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    // hero slider
+    $('#hero-slider').owlCarousel({
+        loop: true,
+        nav: true,
+           items: 1,
+        responsive: {
+            0: {
+             
+            },
+            600: {
+              
+            },
+            1000: {
+              
+            }
+        }
+    });
+});
