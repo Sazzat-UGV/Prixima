f26e8a721842d8bc0346cdf22d8d15467324005f
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-06-22 13:11:16 +0600

    working with project

--- js/app.js
+++ js/app.js
@@ -4,14 +4,34 @@ $(document).ready(function () {
     loop: true,
     nav: true,
     dots: false,
-    navText:['PREV', 'NEXT'],
+    navText: ["PREV", "NEXT"],
     items: 1,
     smartSpeed: 1000,
     autoplay: true,
     responsive: {
+      0: {
+        nav: false,
+      },
+      768: {
+        nav: true,
+      },
+    },
+  });
+  // project slider
+  $("#project-slider").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    smartSpeed: 1000,
+    autoplay: true,
+    margin: 24,
+    responsive: {
       0: {},
-      600: {},
-      1000: {},
+      768: {},
+      1140: {
+        items: 2,
+        center: true
+      },
     },
   });
 });
