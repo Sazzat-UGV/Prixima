c4d9f56efc7a63d68c8414b77dc2b16dff64863a
Asikul Islam Sazzat <asikulislamsazzat@gmail.com>
2025-06-22 21:36:45 +0600

    working with team section

--- js/app.js
+++ js/app.js
@@ -20,16 +20,24 @@ $(document).ready(function () {
   // project slider
   $("#project-slider").owlCarousel({
     loop: true,
-    nav: false,
-    dots: false,
+    nav: true,
     smartSpeed: 1000,
     autoplay: true,
     margin: 24,
+    navText: ["PREV", "NEXT"],
     responsive: {
-      0: {},
-      768: {},
+      0: {
+        items: 1,
+        nav: false,
+        margin: 0,
+        dots: false,
+      },
+      768: {
+        items: 2,
+      },
       1140: {
         items: 2,
+        dots: true,
         center: true
       },
     },
